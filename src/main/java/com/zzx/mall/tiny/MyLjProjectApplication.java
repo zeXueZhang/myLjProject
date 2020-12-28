0c6ba74a64f3ab29cc4765bd0d09d06615f12391
zhangzexue <1010345457@qq.co>
2020-12-28 09:14:09 +0800

    首次提交

---
+++ src/main/java/com/zzx/mall/tiny/MyLjProjectApplication.java
@@ -0,0 +1,14 @@
+package com.zzx.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyLjProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLjProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzx/demo/MyLjProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzx.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLjProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
